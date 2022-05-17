20cc17d48edc1a337271b0f759b3ea9c8ddd07e3
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-05-17 12:35:30 +0300

    touchbackend added for mobile

--- kanban/src/App.tsx
+++ kanban/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header/Header';
@@ -9,9 +10,11 @@ import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 
 function App() {
+  const isMobile = window.innerWidth < 768
+
   return (
     <div className="App">
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
         <header>
           <Header />
         </header>
